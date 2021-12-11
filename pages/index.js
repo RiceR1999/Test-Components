fb5f92383df7f7fc12ef6c5e50815a9a644f0473
RyanR <ricer3@students.rowan.edu>
2021-12-10 20:04:37 -0500

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,66 @@
+import { data } from '../SpeakerData';
+
+const {id, first, last, company, bio, twitterHandle, favorite, sessions} = data[0];
+
+const Session = ({title, room}) => {
+    
+    // const {title, room} = props;
+    // const title = props.title;
+    // const room = props.room;
+
+    return (
+      <span className='session w-100'>
+        {title}<strong>Room: {room}</strong>
+      </span>
+    ); 
+}
+
+const IndexPage = () => {
+    return (
+        <div className='container speakers-list'> 
+          <div className="row">
+              {data.map((speaker) => {
+                  const {id, 
+                    first, 
+                    last, 
+                    company, 
+                    bio, 
+                    twitterHandle, 
+                    favorite, 
+                    sessions} = speaker;
+                    
+                    return(
+                    <div 
+                    key={id}
+                    className='col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12'>
+                    <div className="card card-height p-4 mt-4">
+                    <div className='speaker-img d-flex flex-row justify-content-center align-items-center h-300'>
+                          <img className='contain-fit' src={`/images/speaker-${id}.jpg`}
+                          width='300'
+                          alt={`${first} ${last}`}/>
+                    </div>
+                    
+                    <div className='speaker-info'>
+                        <div className='d-flex justify-content-between mb-3'>
+                              <h3 className='text-truncate w-200'> 
+                                  {first} {last}
+                              </h3>
+                        </div>
+                        <div>
+                            <p>{bio} {company} {twitterHandle} {favorite}</p>
+                        </div>
+                        </div>
+                       </div>
+                    <div className='sessionBox card h-250'>
+                        <Session title={sessions[0].title} room={sessions[0].room.name}/>
+                    </div>         
+                  </div>
+              )})}
+            </div>
+         </div>
+         
+    );
+}
+
+
+export default IndexPage;
