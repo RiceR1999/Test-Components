f87940e99e9855606a2533bb877b845941521c1e
Ryan Rice <ricer3@students.rowan.edu>
2021-12-13 11:07:10 -0500

    add loading spinner, and delay function to simulate API call

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,23 @@
+const LoadingSpinner = ({isLoading}) => {
+    return (
+    isLoading === true ?
+        <div className="svgMain">
+            <svg width="51px" height="50px" viewBox="0 0 51 50">
+                <rect y="0" width="50" height="13" fill="#1fa2ff">
+                    <animate attributeName="width" values="50;10;50" begin="0s" dur="1s" repeatCount="indefinite" />
+                    <animate attributeName="x" values="0;20;0" begin="0s" dur="1s" repeatCount="indefinite" />
+                </rect>
+                <rect x="0 " y="19" width="50" height="13" fill="#12d8fa">
+                    <animate attributeName="width" values="50;10;50" begin="0.2s" dur="1s" repeatCount="indefinite" />
+                    <animate attributeName="x" values="0;20;0" begin="0.2s" dur="1s" repeatCount="indefinite" />
+                </rect>
+                <rect x="0" y="38" width="50" height="13" fill="#06ffcb">
+                    <animate attributeName="width" values="50;10;50" begin="0.4s" dur="1s" repeatCount="indefinite" />
+                    <animate attributeName="x" values="0;20;0" begin="0.4s" dur="1s" repeatCount="indefinite" />
+                </rect>
+            </svg>
+            </div>
+            : <> </>
+    );
+}
+export default LoadingSpinner;
--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -1,11 +1,40 @@
 import Speaker from './Speaker';
 import { data } from '../../SpeakerData';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import LoadingSpinner from './LoadingSpinner';
+import ReactPlaceholder from 'react-placeholder/lib';
 
 const SpeakersList = ({showSessions}) => {
     
-    const [speakersData, setSpeakersData] = useState(data);
+    const [speakersData, setSpeakersData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasErrored, setHasErrored] = useState(false);
+    const [error, setError] = useState("");
     
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    useEffect(() => {
+        async function delayFunc() {
+            try {
+                await delay(2000);
+                // throw "Had Error ."
+                setIsLoading(false);
+                setSpeakersData(data);
+            }
+            catch (e) {
+                setIsLoading(false);
+                setHasErrored(true);
+                setError(e);
+            }
+        }
+        delayFunc();
+    }, []);
+
+    if (hasErrored === true) {
+        return (
+            <div className="text-danger">Error:<b> loading Speaker Data Failed</b></div>
+        );
+    }
+
     const onFavoriteToggle = (id) => {
     
         const speakersRecPrevious = speakersData.find((rec) => {
@@ -25,8 +54,11 @@ const SpeakersList = ({showSessions}) => {
     }
 
     return (
-        <div className='container speakers-list'> 
-          <div className="row">
+        <div className='container speakers-list'>
+            {/* <ReactPlaceholder type="media" rows={15} className="speakerslist-placeholder"
+                ready={isLoading === false}/> DIS SHIT BROKE */}
+            <div className="row">
+                <LoadingSpinner isLoading={isLoading}/>
               {speakersData.map((speaker) => {
                     return(<Speaker key={speaker.id} 
                         speaker={speaker} 
